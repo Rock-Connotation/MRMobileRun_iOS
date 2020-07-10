df72be4022ecf96a4a422381112c44c3a8dd25ae
mgd666 <1291788563@qq.com>
2020-07-10 10:05:50 +0800

    我的 页面的顶部视图

---
+++ MRMobileRun/MRMineView/View/MGDTopView.h
@@ -0,0 +1,27 @@
+//
+//  MGDTopView.h
+//  MRMobileRun
+//
+//  Created by 阿栋 on 2020/7/9.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@class User;
+
+@interface MGDTopView : UIView
+
+@property(nonatomic,strong) UIView *topView;
+
+@property(nonatomic,strong) UIImageView *userIcon;
+
+@property(nonatomic,strong) UILabel *userName;
+
+@property(nonatomic,strong) UILabel *personalSign;
+
+@property (nonatomic, strong) User *user;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MRMobileRun/MRMineView/View/MGDTopView.m
@@ -0,0 +1,100 @@
+//
+//  MGDTopView.m
+//  MRMobileRun
+//
+//  Created by 阿栋 on 2020/7/9.
+//
+
+#import "MGDTopView.h"
+#import <Masonry.h>
+
+@implementation MGDTopView
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        UIView *topView = [[UIView alloc] init];
+        self.topView = topView;
+        self.topView.backgroundColor = [UIColor colorWithRed:255/255.0 green:255/255.0 blue:255/255.0 alpha:1.0];
+        [self addSubview:topView];
+        
+        UIImageView *userIcon = [[UIImageView alloc] init];
+        self.userIcon = userIcon;
+        self.userIcon.layer.masksToBounds = YES;
+        self.userIcon.layer.cornerRadius = 36.0;
+        [self.topView addSubview:userIcon];
+        
+        UILabel *username = [[UILabel alloc] init];
+        self.userName = username;
+        [self.topView addSubview:username];
+        _userName.numberOfLines = 0;
+        _userName.font = [UIFont fontWithName:@"PingFangSC-Medium" size: 18];
+        _userName.textColor = [UIColor colorWithRed:51/255.0 green:55/255.0 blue:57/255.0 alpha:1.0];
+        
+        UILabel *personalSign = [[UILabel alloc] init];
+        self.personalSign = personalSign;
+        [self.topView addSubview:personalSign];
+        _personalSign.font =  [UIFont fontWithName:@"PingFangSC-Regular" size: 13];
+        _personalSign.textColor = [UIColor colorWithRed:160/255.0 green:160/255.0 blue:160/255.0 alpha:1.0];
+        _personalSign.numberOfLines = 0;
+        
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    [_topView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self).mas_offset(0);
+        make.bottom.equalTo(self).mas_offset(-676);
+        make.height.equalTo(@136);
+        make.width.mas_equalTo(screenWidth);
+    }];
+    
+    [_userIcon mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.equalTo(@72);
+        make.width.equalTo(@72);
+        make.right.equalTo(self.topView.mas_left).mas_offset(-287);
+        make.left.equalTo(self.topView.mas_left).mas_offset(16);
+        make.top.equalTo(self.topView.mas_top).mas_offset(48);
+        make.bottom.equalTo(self.topView.mas_bottom).mas_offset(-16);
+    }];
+    
+    [_userName mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.equalTo(@25);
+        make.width.equalTo(@127);
+        make.right.equalTo(self.topView.mas_left).mas_offset(-132);
+        make.left.equalTo(self.topView.mas_left).mas_offset(116);
+        make.top.equalTo(self.topView.mas_top).mas_offset(59);
+        make.bottom.equalTo(self.topView.mas_bottom).mas_offset(-52);
+    }];
+    
+    [_personalSign mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.equalTo(@18);
+        make.width.equalTo(@212);
+        make.right.equalTo(self.topView.mas_left).mas_offset(-47);
+        make.left.equalTo(self.topView.mas_left).mas_offset(116);
+        make.top.equalTo(self.topView.mas_top).mas_offset(84);
+        make.bottom.equalTo(self.topView.mas_bottom).mas_offset(-34);
+    }];
+    
+    
+    //测试头像
+    _userIcon.image = [UIImage imageNamed:@"avatar"];
+    
+    //测试昵称
+    _userName.text = @"你的寒王";
+    
+    //测试签名
+    _personalSign.text = @"花花世界迷人眼,没有实力你别赛脸!";
+}
+
+- (void)setUser:(User *)user {
+    _user = user;
+    
+}
+
+
+
+
+@end
