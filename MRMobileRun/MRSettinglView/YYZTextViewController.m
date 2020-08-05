166573bba909040b0f6501e6ab2e19db36f176b2
杨远舟 <yyz@YYZ-MacBook-Pro.local>
2020-08-06 01:22:21 +0800

    更新我的页面

---
+++ MRMobileRun/MRSettinglView/YYZTextViewController.m
@@ -0,0 +1,31 @@
+//
+//  YYZTextViewController.m
+//  MRMobileRun
+//
+//  Created by 杨远舟 on 2020/7/31.
+//
+
+#import "YYZTextViewController.h"
+
+@interface YYZTextViewController ()
+
+@end
+
+@implementation YYZTextViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
