84007b49a44d440447c9e56febd9b7771bc32443
Rock-Connotation <2125387192@qq.com>
2020-08-20 19:38:15 +0800

    上传gyy丢失的文件

---
+++ MRMobileRun/MRMainView/GYYRankModel.h
@@ -0,0 +1,24 @@
+//
+//  GYYRankModel.h
+//  MRMobileRun
+//
+//  Created by 郭蕴尧 on 2020/8/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GYYRankModel : NSObject
+
+@property (nonatomic, copy)NSString *AvatarUrl;
+@property (nonatomic, copy)NSString *Name;
+@property (nonatomic, copy)NSString *Nickname;
+@property (nonatomic, copy)NSString *Rank;
+@property (nonatomic, copy)NSString *Signature;
+@property (nonatomic, copy)NSString *Unit;
+@property (nonatomic, copy)NSString *DistanceValue;   //Vaule
+
+@end
+
+NS_ASSUME_NONNULL_END
